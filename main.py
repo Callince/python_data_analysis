bb433100892da89460c162f9d8cd878a22ad9769
Callince Goodson M <126311015+Callince@users.noreply.github.com>
2024-02-21 17:00:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from mean_var_std import calculate
+
+result= calculate([0,1,2,3,4,5,6,7,8])
+print(result)
---
+++ test_module.py
@@ -0,0 +1,16 @@
+import unittest
+from mean_var_std import calculate
+
+class TestCalculateFunction(unittest.TestCase):
+
+      def test_example(self):
+        result = calculate([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        expected_result = {
+            'mean': [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0],
+            'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
+            'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
+            'max': [[6, 7, 8], [2, 5, 8], 8],
+            'min': [[0, 1, 2], [0, 3, 6], 0],
+            'sum': [[9, 12, 15], [3, 12, 21], 36]
+        }
+        self.assertEqual(result, expected_result)
